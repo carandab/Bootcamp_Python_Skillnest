4aac9ae30e10f0d6b1d230e63c90a1e915b5d067
Cristian Aranda <c.aranda.borquez@gmail.com>
2025-07-25 21:33:37 -0400

    Se modifica el Ejercicio Individual de AE2

--- Modulo_4/AE2_ABP_EjercicioIndividual/main.py
+++ Modulo_4/AE2_ABP_EjercicioIndividual/main.py
@@ -18,10 +18,18 @@ if __name__ == '__main__':
     #Para la segunda tarjeta, haz 3 compras y 2 pagos. Luego cobra los intereses
     #  y muestra la información de la tarjeta; todo esto en una sola línea a través de la encadenación.
     print("=== Tarjeta 2 ===\n")
-    tarjeta2.compra(100).compra(200).compra(300).pago(150).pago(100).cobrar_interes().mostrar_info_tarjeta()
+    tarjeta2.compra(100).compra(200).compra(300).pago(150).pago(1000).cobrar_interes().mostrar_info_tarjeta()
 
 
     #Para la tercera tarjeta, haz 5 compras y excede su límite de crédito.
     #  Luego muestra la información de la tarjeta; todo esto en una sola línea a través de la encadenación.
     print("=== Tarjeta 3 ===\n")
     tarjeta3.compra(500).compra(600).compra(300).compra(400).compra(500).mostrar_info_tarjeta()
+
+    #BONUS: crea un método de clase para imprimir todas las instancias de la información de las tarjetas creadas.
+    # En el capítulo pasado te dimos algunas pistas de cómo realizarlo.
+
+    print("\n")
+    print("============== Bonus ==============")
+    print("=== Información de las tarjetas ===")
+    TarjetaCredito.imprimir_info_tarjetas()
--- Modulo_4/AE2_ABP_EjercicioIndividual/tarjeta_credito.py
+++ Modulo_4/AE2_ABP_EjercicioIndividual/tarjeta_credito.py
@@ -1,20 +1,22 @@
 class TarjetaCredito:
 
+    tarjetas = []
 
     def __init__(self, saldo_pagar, limite_credito, intereses):
 
         self.saldo_pagar = saldo_pagar
         self.limite_credito = limite_credito
         self.intereses = intereses
+        TarjetaCredito.tarjetas.append(self)
 
     def compra(self, monto):
 
         if (self.saldo_pagar + monto) > self.limite_credito:
-            print("Tarjeta excede límite")
+            print(f"Tarjeta excede límite. No se ha realizado el cobro de {monto}")
 
         else:
             self.saldo_pagar = self.saldo_pagar + monto
-            print(f"Compra realizada con exito por ${monto}")
+           # print(f"Compra realizada con exito por ${monto}")
 
         return self
 
@@ -24,11 +26,11 @@ class TarjetaCredito:
         if monto >= self.saldo_pagar:
             monto_cobrado = self.saldo_pagar
             self.saldo_pagar = 0
-            print(f"Pago realizado por ${monto_cobrado}. Saldo liquidado completamente.")
+            print(f"Pago realizado por ${monto_cobrado}")
 
         else:
             self.saldo_pagar = self.saldo_pagar - monto
-            print(f"Pago realizado por ${monto}")
+            #print(f"Pago realizado por ${monto}")
 
         return self
 
@@ -47,3 +49,12 @@ class TarjetaCredito:
         print("\n")
 
         return self
+
+    @classmethod
+    def imprimir_info_tarjetas(cls):
+
+        for i, tarjeta in enumerate(cls.tarjetas, 1):
+            print("\n")
+            print(f"Tarjeta {i}:")
+            tarjeta.mostrar_info_tarjeta()
+        return cls
